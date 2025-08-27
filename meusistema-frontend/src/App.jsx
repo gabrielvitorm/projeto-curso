e65fae2bc66be1675b27561ee089c28326388dcf
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2025-08-27 12:39:02 -0300

    fix: Navbar ongoing

--- meusistema-frontend/src/App.jsx
+++ meusistema-frontend/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import FornecedorForm from './pages/Fornecedor/FornecedorForm'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Inicial from './pages/Inicial'
+import Menu from './components/Menu'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Menu/>
       <Routes>
         <Route path='/' element={<Inicial />}/>
         <Route path='/cadastrar-fornecedor' element={<FornecedorForm />}/>
