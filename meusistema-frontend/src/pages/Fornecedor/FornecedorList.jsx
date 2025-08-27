f4be824c85886a97f25780f165bf21fac76deb7b
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2025-08-27 12:57:28 -0300

    fix: Botão add fornecedor

--- meusistema-frontend/src/pages/Fornecedor/FornecedorList.jsx
+++ meusistema-frontend/src/pages/Fornecedor/FornecedorList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Container, OverlayTrigger, Tooltip } from 'react-bootstrap'
-import { FaQuestionCircle } from 'react-icons/fa'
+import { Button, Container, OverlayTrigger, Tooltip } from 'react-bootstrap'
+import { FaPlus, FaQuestionCircle } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const FornecedorList = () => {
   return (
@@ -17,6 +18,14 @@ const FornecedorList = () => {
 
         </OverlayTrigger>
       </h2>
+
+      <div className='mb-3'>
+        <Button as={Link} to='/cadastrar-fornecedor' variant='primary'>
+          <FaPlus className='me-2'/>
+          Adicionar Fornecedor
+        </Button>
+
+      </div>
     </Container>
   )
 }
