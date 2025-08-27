621847fdcc763235906f30dbd35527915394ec0d
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2025-08-27 12:52:06 -0300

    fix: FornecedorList inicio

--- meusistema-frontend/src/App.jsx
+++ meusistema-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import FornecedorForm from './pages/Fornecedor/FornecedorForm'
+import FornecedorList from './pages/Fornecedor/FornecedorList'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Inicial from './pages/Inicial'
 import Menu from './components/Menu'
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Inicial />}/>
         <Route path='/cadastrar-fornecedor' element={<FornecedorForm />}/>
+        <Route path='/listar-fornecedores' element={<FornecedorList />} />
       </Routes>
     </BrowserRouter>
   )
